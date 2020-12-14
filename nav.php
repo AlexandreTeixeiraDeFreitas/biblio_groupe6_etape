55d29663bf8adf8f7132317c4089cfe57437fb92
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2020-12-14 10:47:57 +0100

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,6 +2,6 @@
         <ul>
             
             <li><a href="\biblio_groupe6_etape2\biblio_groupe6_etape2\biblio_groupe6_etape\index.php">Accueil</a></li>
-            <li><a href="\biblio_groupe6_etape2\biblio_groupe6_etape2\biblio_groupe6_etape\contact.php">contact</a></li>
+            <li><a href="\biblio_groupe6_etape2\biblio_groupe6_etape2\iblio_groupe6_etape\contact.php">contact</a></li>
         </ul>
     </nav>
