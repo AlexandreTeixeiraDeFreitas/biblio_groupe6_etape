0ccd8bebea2648ce4889656e2b32247b0dd19343
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2020-12-14 19:59:03 +0100

    Update foulder.php

--- foulder.php
+++ foulder.php
@@ -8,5 +8,5 @@
     <link rel="stylesheet" href="\biblio_groupe6_etape2\biblio_groupe6_etape2\biblio_groupe6_etape\style\nav.css">
 </head>
 <body>
-    <a class=icon href="index.php"><img alt="Logo" src="\biblio_groupe6_etape2\biblio_groupe6_etape2\biblio_groupe6_etape\img\Manga_Whale.png"></a>
-    <h1>Manga kyōjin</h1>
+<h1><a class=icon href="index.php"><img alt="Logo" src="\biblio_groupe6_etape2\biblio_groupe6_etape2\biblio_groupe6_etape\img\Manga_Whale.png"></a>
+    Manga kyōjin</h1>
